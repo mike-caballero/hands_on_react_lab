20698ebfdaf8242034a3402011bc200ffa3382bd
Mike Caballero <michaelstephencaballero@gmail.com>
2023-12-11 22:01:02 -0800

    Setup react lab repo with lab A code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ProjectsPage from './projects/ProjectsPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ProjectsPage />
+    </div>
+  );
+}
+
+export default App;
