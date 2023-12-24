2aef786ac59b59a5b74def784b6229fe1746de85
Mike Caballero <michaelstephencaballero@gmail.com>
2023-12-24 14:54:25 -0500

    Finish Lab E and check in code

--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -80,6 +80,13 @@ const projectAPI = {
             );
         });
   },
+
+  find(id: number) {
+    return fetch(`${url}/${id}`)
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(convertToProjectModel);
+  },
 };
 
 export { projectAPI };
